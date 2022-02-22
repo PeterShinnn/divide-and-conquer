8217b3854c5790bbc436930fcd661a4cf2e11a4f
Peter Shin <petershin731@gmail.com>
2022-02-21 16:58:24 -0800

    added the link to work properly

--- react-app/src/components/NavBar/SideNavBar.js
+++ react-app/src/components/NavBar/SideNavBar.js
@@ -17,13 +17,13 @@ function SideNavBar() {
             <div className="side-nav-bar-container">
                 <ul className="menu-items-btn">
                     <div><li><Link to='/taskboard'><img id="website-logo" src={smallerLogo} alt="website-logo" /></Link></li></div>
-                    <div><li><Link to='#'><i className="fa-solid fa-table-cells-large nav-icon"></i></Link></li></div>
-                    <div><li><Link to='#'><i className="fa-brands fa-linkedin nav-icon"></i></Link></li></div>
-                    <div><li><Link to='#'><i className="fa-brands fa-github-square nav-icon"></i></Link></li></div>
+                    <div><li><Link to='/taskboard'><i className="fa-solid fa-table-cells-large nav-icon"></i></Link></li></div>
+                    <div><li><Link to={{ pathname: "https://www.linkedin.com/in/petershin731/" }} target="_blank"><i className="fa-brands fa-linkedin nav-icon"></i></Link></li></div>
+                    <div><li><Link to={{ pathname: "https://github.com/PeterShinnn" }} target="_blank"><i className="fa-brands fa-github-square nav-icon"></i></Link></li></div>
                 </ul>
                 <div>
                     <div className="side-nav-footer">
-                        <div><i className="fa-brands fa-github-alt nav-icon"></i></div>
+                        <div><Link to={{pathname: "https://github.com/PeterShinnn/divide-and-conquer"}} target="_blank"><i className="fa-brands fa-github-alt nav-icon"></i></Link></div>
                         <div><i onClick={()=>onLogout()} className="fa-solid fa-arrow-right-from-bracket nav-icon logout-btn"></i></div>
                     </div>
                 </div>
